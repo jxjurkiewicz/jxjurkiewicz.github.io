ed045cba91b6782d66d8ee84ca43f436ea8596c6
jxjurkiewicz <jxjurkiewicz@gmail.com>
2024-01-16 18:24:05 +0100

    og-image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  env: {
+    metadataBase: "https://jxjurkiewicz.github.io",
+  },
 };
 
 module.exports = nextConfig;
