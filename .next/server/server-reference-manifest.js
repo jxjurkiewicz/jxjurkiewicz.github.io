1804445fdeda1433e11f4cabce0414c977b8bd99
jxjurkiewicz <jxjurkiewicz@gmail.com>
2024-06-17 17:20:50 +0200

    run build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"noUe9MuEtOBlWCX2EonzbEKadGEnnJsV36yxl7LbsSU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"BrIEd0qObYqsd+RWkcDSsGIWl8lmtmbNrl9eFJyTlBk=\"}"
